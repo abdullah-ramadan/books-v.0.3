762f318e6630b096c1957543bd693740ec93d620
abdullah ramdan salem <abdullahramdan01@gmail.com>
2018-11-30 23:04:32 +0300

    enhancing http response in case of auth failed
    enhancing http response in case of auth failed

---
+++ exchange/src/main/java/org/book/exchange/config/SecurityConfig.java
@@ -0,0 +1,28 @@
+package org.book.exchange.config;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
+
+@Configuration
+@EnableWebSecurity
+public class SecurityConfig extends WebSecurityConfigurerAdapter {
+
+  @Autowired AuthenticationEntryPoint authenticationEntryPoint;
+
+  @Override
+  protected void configure(HttpSecurity http) throws Exception {
+    http.cors()
+        .and()
+        .csrf()
+        .disable()
+        .authorizeRequests()
+        .anyRequest()
+        .fullyAuthenticated()
+        .and()
+        .httpBasic()
+        .authenticationEntryPoint(authenticationEntryPoint);
+  }
+}
